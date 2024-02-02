c50017c47268afe1ae5537aaf0478cc3e401e203
barrodev <barrodev@gmail.com>
2024-02-02 02:09:15 +0000

    Monsieur Le Testeur

--- src/app/blogs/users/users.component.ts
+++ src/app/blogs/users/users.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  const myfirts = document.getElementById("mincon"); 
+  //const myfirts = document.getElementById("mincon"); 
 
 
 }
